70cbf939779bd83a399bf8e697d5d7c2db4f8ee7
edilsonjrcode <2022111510116@iesp.edu.br>
2025-01-25 00:34:36 -0300

    add: página de sobre

---
+++ src/components/About/index.jsx
@@ -0,0 +1,10 @@
+import * as Styled from "./style.js";
+
+export const About = () => {
+
+  return (
+    <>
+      <Styled.Container>About</Styled.Container>
+    </>
+  );
+};
---
+++ src/components/About/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { theme } from "../../style/theme";
+
+export const Container = styled.section`
+    background-color: ${theme.colors.red};
+    height: 100vh;
+    width: 100%;
+`
