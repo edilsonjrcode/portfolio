03829ee6a88b800d8c38c2741aead14f5ae48f6e
edilsonjrcode <2022111510116@iesp.edu.br>
2025-01-25 00:34:47 -0300

    add: página principal

---
+++ src/components/Principal/index.jsx
@@ -0,0 +1,10 @@
+import * as Styled from "./style.js";
+
+export const Principal = () => {
+
+  return (
+    <>
+      <Styled.Container>Principal</Styled.Container>
+    </>
+  );
+};
---
+++ src/components/Principal/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { theme } from "../../style/theme";
+
+export const Container = styled.section`
+    background-color: ${theme.colors.black};
+    height: 100vh;
+    width: 100%;
+    color: ${theme.colors.white};
+    padding-top: 7rem;
+`
