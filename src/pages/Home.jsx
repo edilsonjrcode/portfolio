75f83ed8489af0cd6dcf8bd1e925189a41f59972
edilsonjrcode <2022111510116@iesp.edu.br>
2025-01-25 23:15:46 -0300

    add: adiciona as novas seções à página

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { About } from "../components/About"
 import { Header } from "../components/Header"
 import { Principal } from "../components/Principal"
+import { Skills } from "../components/Skills"
 
 function Home() {
 
@@ -8,7 +9,9 @@ function Home() {
     <>
       <Header/>
       <Principal/>
+      <div style={{width: '100%', height: '9.8125rem', background:"#FF003D"}}/>
       <About/>
+      <Skills/>
     </>
   )
 }
