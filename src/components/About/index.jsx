23f89966a814df8e3630610e0bb94486148c8832
edilsonjrcode <2022111510116@iesp.edu.br>
2025-01-25 23:12:46 -0300

    add: estrutura inicial da seção about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,10 +1,24 @@
+import { Title } from "../Title/index.jsx";
 import * as Styled from "./style.js";
+import imageJrAbout from '../../assets/photoJrAbout.png'
+import * as SharedStyle from "../../style/sharedStyles.js";
 
 export const About = () => {
 
   return (
     <>
-      <Styled.Container>About</Styled.Container>
+      <Styled.Container>
+        <Styled.BoxAbout>
+          <Styled.PessoalImg src={imageJrAbout} alt="Foto Edilson Junior" />
+          <Styled.BoxTexts>
+            <Title text="Sobre" align="right" margin="1.75rem"/>
+            <SharedStyle.ParagrathSections align="right" widthrectangle="36.1875rem">Olá, meu nome é Edilson Júnior, sou Desenvolvedor Frontend com 2 anos de experiência. Moro em João Pessoa - PB, e sou apaixonado por criar interfaces modernas, intuitivas e acessíveis. Minha jornada como desenvolvedor começou com um forte interesse em transformar ideias em experiências digitais cativantes.
+            <br/><br/>Nos últimos dois anos, trabalhei com diversas tecnologias e sempre me desafiei a aprender novas ferramentas para contribuir na entrega de soluções inovadoras.</SharedStyle.ParagrathSections>
+            <br/>
+            <SharedStyle.Rectangle widthrectangle="10.25rem" heigthrectangle="0.6875rem" color="red"/>
+          </Styled.BoxTexts>
+        </Styled.BoxAbout>
+      </Styled.Container>
     </>
   );
 };
--- src/components/About/style.js
+++ src/components/About/style.js
@@ -1,8 +1,29 @@
 import styled from "styled-components";
 import { theme } from "../../style/theme";
+import { BoxFlex } from "../../style/mixins";
+
+export const BoxAbout = styled.div`
+    ${() => BoxFlex('flex', 'row', 'space-between', 'center')}
+    position: relative;
+
+`
+export const BoxTexts = styled.div`
+    ${() => BoxFlex('flex', 'column', 'space-between', 'flex-end')}
+    position: absolute;
+    right: 0;
+`
 
 export const Container = styled.section`
-    background-color: ${theme.colors.red};
+    background-color: ${theme.colors.black};
     height: 100vh;
     width: 100%;
+    padding: 0rem 4.75rem 0rem 2.25rem;
+    overflow: hidden;
+`
+
+export const PessoalImg = styled.img`
+    width: auto;
+    height: 100vh;
+    object-fit: cover;
+    padding: 6rem 0rem 0rem 0rem;
 `
