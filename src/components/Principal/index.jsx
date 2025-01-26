eaa23d92fbc2f733d4370419950b3e509bb2059d
edilsonjrcode <2022111510116@iesp.edu.br>
2025-01-25 23:15:10 -0300

    add: estrutura inicial da seção principal

--- src/components/Principal/index.jsx
+++ src/components/Principal/index.jsx
@@ -1,10 +1,45 @@
 import * as Styled from "./style.js";
+import * as SharedStyle from "../../style/sharedStyles.js";
+import photoJr from '../../assets/photoJr.png'
+import githubIcon from '../../assets/socialMedia/githubIcon.svg'
+import googleIcon from '../../assets/socialMedia/googleIcon.svg'
+import instagramIcon from '../../assets/socialMedia/instagramIcon.svg'
+import linkedinIcon from '../../assets/socialMedia/linkedinIcon.svg'
+import { Button } from "../Button/index.jsx";
+import { Title } from "../Title/index.jsx";
 
 export const Principal = () => {
 
   return (
     <>
-      <Styled.Container>Principal</Styled.Container>
+      <Styled.Container>
+        <Styled.BoxPrincipal display="flex" flexdirection="row" justifycontent="space-between" alignitems="center">
+          <Styled.Information display="flex" flexdirection="column" justifycontent="flex-start" alignitems="flex-start">
+            <Styled.BoxHello display="flex" flexdirection="row" justifycontent="flex-start" alignitems="center">
+              <SharedStyle.Rectangle widthrectangle="5.5625rem" heigthrectangle="0.6875rem" color="red" marginright="1rem"/>
+              Olá, me chamo Edilson Júnior
+            </Styled.BoxHello>
+
+            <Title text="Frontend Developer" margin="4rem"/>
+
+            <Styled.BoxApresentation display="flex" flexdirection="row" justifycontent="flex-start" alignitems="flex-start">
+              <SharedStyle.Rectangle widthrectangle="1.34544rem;" heigthrectangle="11.3rem" color="red" marginright="1rem"/>
+              <Styled.BoxViewPortfolio>
+                <Styled.SocialMedia>
+                  <img src={linkedinIcon} alt="Icone Linkedin"/>
+                  <img src={instagramIcon} alt="Icone Instagram"/>
+                  <img src={githubIcon} alt="Icone Github"/>
+                  <img src={googleIcon} alt="Icone Google"/>
+                </Styled.SocialMedia>
+                <Styled.Paragraph>Sou um desenvolvedor frontend apaixonado por criar interfaces dinâmicas e experiências web incríveis. Abaixo você encontrará alguns dos projetos que refletem meu trabalho, meu aprendizado e minha dedicação.
+                Vamos explorar juntos?</Styled.Paragraph>
+                <Button text="Ver Portfólio"/>
+              </Styled.BoxViewPortfolio>
+            </Styled.BoxApresentation>
+          </Styled.Information>
+          <Styled.PessoalImg src={photoJr} alt="" />
+        </Styled.BoxPrincipal>
+      </Styled.Container>
     </>
   );
 };
--- src/components/Principal/style.js
+++ src/components/Principal/style.js
@@ -1,10 +1,54 @@
 import styled from "styled-components";
 import { theme } from "../../style/theme";
+import { BoxFlex } from "../../style/mixins";
+
+export const BoxViewPortfolio = styled.div`
+    ${() => BoxFlex('flex', 'column', 'flex-start', 'flex-start')}
+`
+export const BoxApresentation = styled.div`
+    ${() => BoxFlex('flex', 'row', 'flex-start', 'flex-start')}
+`
+export const BoxHello = styled.div`
+    ${() => BoxFlex('flex', 'row', 'flex-start', 'center')}
+`
+export const Information = styled.div`
+    ${() => BoxFlex('flex', 'column', 'flex-start', 'flex-start')}
+    width: 37.25rem;
+`
+export const BoxPrincipal = styled.div`
+    ${() => BoxFlex('flex', 'row', 'space-between', 'center')}
+    position: relative;
+`
 
 export const Container = styled.section`
     background-color: ${theme.colors.black};
     height: 100vh;
     width: 100%;
-    color: ${theme.colors.white};
-    padding-top: 7rem;
+    padding: 0rem 6rem;
+    overflow: hidden;
+`
+
+export const Paragraph = styled.h1`
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 300;
+    line-height: 1.25rem;
+    width: 29.25rem;
+    margin-bottom: 1rem;
+`
+
+export const PessoalImg = styled.img`
+    width: auto;
+    height: 100vh;
+    object-fit: cover;
+    padding: 6rem 0rem 0rem 0rem;
+`
+
+export const SocialMedia = styled.div`
+    ${() => BoxFlex('flex', 'row', 'space-around', 'center')}
+    img {
+        width: 1.2rem;
+        margin-right: 1rem;
+        margin-bottom: .75rem;
+    }
 `
