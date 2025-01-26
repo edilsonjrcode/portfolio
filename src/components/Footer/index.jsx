af6dd6b0300ed8720af56271cf0a235ba77c45f9
edilsonjrcode <2022111510116@iesp.edu.br>
2025-01-26 16:29:38 -0300

    add: seção de footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import * as Styled from "./style.js";
+
+export const Footer = () => {
+
+  return (
+    <>
+      <Styled.Container>
+        <Styled.Footer>
+          <span>© 2025. Edilson Júnior - Desenvolvedor Frontend 
+          Todos os direitos reservados. Desenvolvido por Edilson Júnior</span>
+        </Styled.Footer>
+      </Styled.Container>
+    </>
+  );
+};
