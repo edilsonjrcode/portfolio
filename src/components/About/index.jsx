7f2eab486fa29c58ef43f8851f49f02a357acc90
edilsonjrcode <2022111510116@iesp.edu.br>
2025-01-26 11:59:12 -0300

    fix: responsividade da seção de about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -12,8 +12,8 @@ export const About = () => {
           <Styled.PessoalImg src={imageJrAbout} alt="Foto Edilson Junior" />
           <Styled.BoxTexts>
             <Title text="Sobre" align="right" margin="1.75rem"/>
-            <SharedStyle.ParagrathSections align="right" widthrectangle="36.1875rem">Olá, meu nome é Edilson Júnior, sou Desenvolvedor Frontend com 2 anos de experiência. Moro em João Pessoa - PB, e sou apaixonado por criar interfaces modernas, intuitivas e acessíveis. Minha jornada como desenvolvedor começou com um forte interesse em transformar ideias em experiências digitais cativantes.
-            <br/><br/>Nos últimos dois anos, trabalhei com diversas tecnologias e sempre me desafiei a aprender novas ferramentas para contribuir na entrega de soluções inovadoras.</SharedStyle.ParagrathSections>
+            <Styled.ParagrathAbout align="right" widthrectangle="36.1875rem">Olá, meu nome é Edilson Júnior, sou Desenvolvedor Frontend com 2 anos de experiência. Moro em João Pessoa - PB, e sou apaixonado por criar interfaces modernas, intuitivas e acessíveis. Minha jornada como desenvolvedor começou com um forte interesse em transformar ideias em experiências digitais cativantes.
+            <br/><br/>Nos últimos dois anos, trabalhei com diversas tecnologias e sempre me desafiei a aprender novas ferramentas para contribuir na entrega de soluções inovadoras.</Styled.ParagrathAbout>
             <br/>
             <SharedStyle.Rectangle widthrectangle="10.25rem" heigthrectangle="0.6875rem" color="red"/>
           </Styled.BoxTexts>
--- src/components/About/style.js
+++ src/components/About/style.js
@@ -1,16 +1,28 @@
 import styled from "styled-components";
 import { theme } from "../../style/theme";
 import { BoxFlex } from "../../style/mixins";
+import { ParagrathSections } from "../../style/sharedStyles";
 
 export const BoxAbout = styled.div`
     ${() => BoxFlex('flex', 'row', 'space-between', 'center')}
     position: relative;
+
+    @media (max-width: 1380px) {
+        ${() => BoxFlex('flex', 'column-reverse', 'space-between', 'center')}
+        width: 100%;
+    }
 
 `
 export const BoxTexts = styled.div`
     ${() => BoxFlex('flex', 'column', 'space-between', 'flex-end')}
     position: absolute;
     right: 0;
+
+    @media (max-width: 1380px) {
+        ${() => BoxFlex('flex', 'column', 'flex-end', 'flex-end')}
+        position: relative;
+        width: 100%;
+    }
 `
 
 export const Container = styled.section`
@@ -19,6 +31,16 @@ export const Container = styled.section`
     width: 100%;
     padding: 0rem 4.75rem 0rem 2.25rem;
     overflow: hidden;
+
+    @media (max-width: 1380px) {
+        height: auto;
+        padding: 7rem 6rem 0rem 6rem;
+    }
+
+    @media (max-width: 768px) {
+        height: auto;
+        padding: 5rem 2rem 0rem 2rem;
+    }
 `
 
 export const PessoalImg = styled.img`
@@ -26,4 +48,20 @@ export const PessoalImg = styled.img`
     height: 100vh;
     object-fit: cover;
     padding: 6rem 0rem 0rem 0rem;
+
+    @media (max-width: 1380px) {
+        height: 50rem;
+        padding: 0rem;
+    }
+
+    @media (max-width: 768px) {
+        height: 30rem;
+        padding: 0rem;
+    }
+`
+
+export const ParagrathAbout = styled(ParagrathSections)`
+    @media (max-width: 1380px) {
+        width: 100%;
+    }
 `
